426fdb14472269ad3dca3d7f2c3919af3aa19651
odevpedro <pedrosschmidt2@gmail.com>
2025-06-09 14:53:47 -0300

    working with lists

--- Api.py
+++ Api.py
@@ -1,10 +1,21 @@
+
 import requests
 
+'montando a chamada na url'
 def fetch_data(endpoint):
     base_url = "https://rickandmortyapi.com/api/"
     response = requests.get(base_url + endpoint)
     return response.json()
 
-character = fetch_data("character")
-print(character)
+'chamando o método definido'
+data = fetch_data("character")
 
+'pego os resultados de data e transformo em collection'
+characters = data["results"]
+
+'intero em cima da coleção que foi gerada'
+for character in characters[:9]:
+    name = character["name"]
+    species = character["species"]
+    status = character["status"]
+    print(f"{name}: {status}")
